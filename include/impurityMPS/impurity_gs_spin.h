f4689730211b942d7183fba0c4df77fd56b6705b
Yuriel <yurielnf@gmail.com>
2026-03-31 15:49:15 +0200

    remove or improve comments

--- include/impurityMPS/impurity_gs_spin.h
+++ include/impurityMPS/impurity_gs_spin.h
@@ -22,47 +22,14 @@ struct Impurity_gs_spin {
     void iterate(DmrgParam args={})
     {
         itensor::cpu_time t0;
-        // std::cout<<fb.p2-fb.p1<<" nActive\n";
-        // fb.occupations_ni2().as_row().eval().print("before repr");
-        // fb.occupations_ni().as_row().eval().print("before repr cc");
         extract_representative(0);
         extract_representative(1);
-        // std::cout<<fb.p2-fb.p1<<" nActive afte repr\n";
-        // std::cout<<"representatives "<<t0.sincemark().wall; t0.mark();
-
-        // fb.occupations_ni2().as_row().eval().print("before dmrg");
-        // fb.occupations_ni().as_row().eval().print("before repr cc");
         doDmrg(args);
-        // fb.print_bond_dims("after dmrg");
-        // std::cout<<" dmrg "<<t0.sincemark().wall; t0.mark();
-        // fb.occupations_ni2().as_row().eval().print("before nat orb");
-        // fb.occupations_ni().as_row().eval().print("before repr cc");
         rotateToNaturalOrbitals();
-        // fb.print_bond_dims("after nat orb");
-        // std::cout<<" NatOrb "<<t0.sincemark().wall<<"\n";
-
-        // for(auto i=0;i<10;i++) doDmrg(args);
     }
-
-    // void iterate2(DmrgParam args={})
-    // {
-    //     itensor::cpu_time t0;
-    //     for(int i=0; ; i+=1) {
-    //         bool r0=extract_representative2(i,0);
-    //         bool r1=extract_representative2(i,1);
-    //         if (!r0 && !r1) break;
-    //         doDmrg(args);
-    //         // std::cout<<i<<" "<< itensor::maxLinkDim(fb.psi)<<" "<<fb.nActive<<" "<<t0.sincemark().wall; t0.mark();
-    //         rotateToNaturalOrbitals();
-    //         // std::cout<<" "<<t0.sincemark().wall<<"\n"; std::cout.flush(); t0.mark();
-    //     }
-    // }
 
     /// extract representative orbital of the sites with ni=nRef where nRef can be 0 or 1
     void extract_representative(int nRef){ fb.extract_representative(K,nRef,true); }
-
-    /// extract representative orbital of the sites with ni=nRef where nRef can be 0 or 1
-    // bool extract_representative2(int sv_index,int nRef){ return fb.extract_representative(sv_index,K,nRef,fb.nActive); }
 
     void doDmrg(DmrgParam args={})
     {
@@ -81,7 +48,7 @@ struct Impurity_gs_spin {
 
     void rotateToNaturalOrbitals()
     {
-        auto [a,b]=fb.interval_active_full(); // it will change
+        auto [a,b]=fb.interval_active_full(); // the interval will change
         auto rot1=fb.rotateToNaturalOrbitals();
         K.cols(a,b-1)=K.cols(a,b-1).eval()*rot1;
         K.rows(a,b-1)=rot1.t()*K.rows(a,b-1).eval();
