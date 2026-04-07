008d08af073f75e513f09c5f0b0c565d610c1533
Yuriel <yurielnf@gmail.com>
2026-04-07 09:31:26 +0200

    complete spin case

--- include/impurityMPS/impurity_dyn_spin.h
+++ include/impurityMPS/impurity_dyn_spin.h
@@ -5,55 +5,74 @@
 #include "tdvp.h"
 #include "basisextension.h"
 
-struct Impurity_dyn {
+struct Impurity_dyn_spin {
     ImpurityParamSpin param;
     double dt;
     arma::cx_mat exp_ih;
     arma::cx_mat Kip0;
-    int nChannel;            ///< the number of channels that connect the impurity with the bath
-
     /// these quantities are updated during the iterations
     Fb_mps_spin<cmpx> fb;        ///< the current few body MPS
     arma::cx_mat K;         ///< the current Hamiltonian
-    // arma::cx_mat Kip;       ///< the current Hamiltonian in the interaction picture of the bath
     double energy=-1000;        // TODO remove energy (or compute it)
 
-    explicit Impurity_dyn(ImpuritySpin const& imp, Fb_mps_spin<cmpx> const& fb_, double dt_=0.1)
+    explicit Impurity_dyn_spin(ImpuritySpin const& imp, Fb_mps_spin<cmpx> const& fb_, double dt_=0.1)
         : param(imp.param)
         , dt(dt_)
         , fb { fb_ }
     {
-        int nImp=param.nImp();
+        //int nImp=param.nImp();
         int L=param.length();
+        arma::real(param.Kmat*1).eval().clean(1e-11).print("Kmat");
         arma::cx_mat K0 = fb.rot * param.Kmat * fb.rot.t(); // original Hamiltonian matrix
         exp_ih = arma::cx_mat(L,L, arma::fill::eye);
-        exp_ih.submat(nImp,nImp, L-1,L-1)=expIH<cmpx>(K0.submat(nImp,nImp, L-1,L-1) * dt);
-        arma::vec s = arma::svd(param.Kmat.submat(0, nImp, nImp-1, L-1));
-        nChannel = arma::find(s>fb.tol*s[0]).eval().size();
-
-        // interaction picture
-        arma::cx_mat K1 = K0.submat(0, nImp, nImp-1, L-1) *
-                          K0.submat(nImp,nImp,L-1, L-1) * arma::cx_double(0,-0.5*dt); //the commutator
         Kip0=K0;
-        Kip0.submat(nImp, nImp, L-1, L-1).fill(0.0);
-        Kip0.submat(0, nImp, nImp-1, L-1)+=K1;
-        Kip0.submat(nImp, 0, L-1, nImp-1)+=K1.t();        
+        for( auto spin : {up,dw}) {
+            auto [a,b] = fb.interval_bath(spin);
+            exp_ih.submat(a,a,b-1,b-1)=expIH<cmpx>(K0.submat(a,a,b-1,b-1) * dt);
+
+            // interaction picture
+            auto [ai,bi] = fb.interval_impurity(spin);
+            arma::cx_mat K1 = K0.submat(ai, a, bi-1, b-1) *
+                              K0.submat(a, a, b-1, b-1) * arma::cx_double(0,-0.5*dt); //the commutator
+            Kip0.submat(a, a, b-1, b-1).fill(0.0);
+            Kip0.submat(ai, a, bi-1, b-1)+=K1;
+            Kip0.submat(a, ai, b-1, bi-1)+=K1.t();
+        }
+        arma::real(Kip0*1).eval().clean(1e-11).print("Kip0");
+        arma::real(fb.rot.t()*Kip0*fb.rot).eval().clean(1e-11).print("Kip0 rotated at constructor");
     }
 
     void iterate(TdvpParam args={})
     {
         // rotate from scratch
-        int nImp=param.nImp();
         K=Kip0;
-        K.rows(0,nImp-1)=K.rows(0,nImp-1).eval()*fb.rot;
-        K.cols(0,nImp-1)=fb.rot.t()*K.cols(0,nImp-1).eval();
+        for(auto spin : {up,dw}) {
+            auto [a,b]=fb.interval_bath(spin);
+            auto [ai,bi]=fb.interval_impurity(spin);
+
+            // K.rows(a,b-1)=K.rows(a,b-1).eval()*fb.rot;
+            // K.cols(a,b-1)=fb.rot.t()*K.cols(a,b-1).eval();
+            // K.submat(ai,a,bi-1,b-1)=K.submat(ai,a,bi-1,b-1).eval()*fb.rot.submat(a,a,b-1,b-1);
+            // K.submat(a,ai,b-1,bi-1)=fb.rot.submat(a,a,b-1,b-1).t()*K.submat(a,ai,b-1,bi-1).eval();
+            K=fb.rot.t()*Kip0*fb.rot;
+        }
         fb.rot=this->exp_ih*fb.rot;   // update of the interaction picture
 
+        arma::real(K*1).eval().clean(1e-11).print("K before repr 0");
+
         extract_representative(0);
+
+        arma::real(K*1).eval().clean(1e-11).print("K before repr 1");
+
         extract_representative(1);
-        extract_representative_final();
-        // evolve();
+        // extract_representative_final();
+
+        arma::real(K*1).eval().clean(1e-11).print("K before tdvp");
+
         doTdvp(args);
+
+        arma::real(K*1).eval().clean(1e-11).print("K before nat orb");
+
         rotateToNaturalOrbitals();
     }
 
@@ -86,7 +105,7 @@ struct Impurity_dyn {
         }
 
         energy = itensor::tdvp(fb.psi,mpo, -imag_1*dt, sweeps,          // TDVP sweep
-                               {"MaxSite",localL,
+                               {"Minb",a+1,"MaxSite",b,
                                 "Truncate", true,
                                 "DoNormalize", true,
                                 "Quiet", true,
